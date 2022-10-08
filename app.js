23419e22897bff3eb405bcdc22a2ac3c940333ef
Eshaan Bhardwaj <55873406+Eshaan-B@users.noreply.github.com>
2022-10-09 00:17:48 +0530

    removed morgan

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 //spins up the express application
 const express = require("express");
 const path = require("path");
-const morgan = require("morgan");
+//const morgan = require("morgan");
 const cors = require("cors");
 require("dotenv").config();
 const session = require("express-session");
@@ -43,7 +43,7 @@ conn.on("error", console.error.bind(console, "connection error:"));
 
 app.set("view engine", "ejs");
 app.set("views", "views");
-app.use(morgan("dev"));
+//app.use(morgan("dev"));
 app.use(cors());
 app.use(express.json({ limit: "16mb" }));
 app.use(express.urlencoded({ extended: false, limit: "16mb" }));
